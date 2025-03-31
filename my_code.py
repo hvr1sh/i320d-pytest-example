d7654b969406e06d56254d30f014cde50aadd4c1
hvr1sh <sharish0416@gmail.com>
2025-03-30 20:57:12 -0500

    Added value error exception

--- my_code.py
+++ my_code.py
@@ -2,6 +2,9 @@ import pytest
 
 def fix_phone_num(phone_num_to_fix):
   # given "5125558823". Split the parts, then recombine and return
+  if (len(phone_num_to_fix) != 10):
+    raise ValueError("Can only format numbers that are exactly 10 digits long")
+  
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
@@ -14,6 +17,8 @@ def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
+  with pytest.raises(ValueError):
+    fix_phone_num("51")
 
 def test_fix_phone_num2():
   assert fix_phone_num("555-442-98761") == "(555) 442 98761"
