190f4c1e16d2f9dbed2167976d5e12d902594761
hvr1sh <sharish0416@gmail.com>
2025-03-30 21:02:24 -0500

    Added extra tests about ValueErrors

--- my_code.py
+++ my_code.py
@@ -5,6 +5,9 @@ def fix_phone_num(phone_num_to_fix):
   if (len(phone_num_to_fix) != 10):
     raise ValueError("Can only format numbers that are exactly 10 digits long")
   
+  if not phone_num_to_fix.isdigit():
+    raise ValueError("All values must be digits")
+  
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
@@ -17,8 +20,15 @@ def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
-  
 
 def test_fix_phone_num2():
   assert fix_phone_num("555-442-98761") == "(555) 442 98761"
   assert fix_phone_num("(321) 654 3333") == "(321) 654 3333"
+
+def test_fixe_phone_num3():
+  with pytest.raises(ValueError):
+    fix_phone_num("51")
+
+def test_fixe_phone_num4():
+  with pytest.raises(ValueError):
+    fix_phone_num("555-442-9876")
